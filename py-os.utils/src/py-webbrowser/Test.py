0b6ebebe131e00a7d5234b86474bc4ae6af7a1b7
sunnychan <sunnnychan@gmail.com>
2019-01-30 16:16:48 +0800

    sunnychan

---
+++ py-os.utils/src/py-webbrowser/Test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time  : 2019-01-30 16:14
+# @Author: sunnnychan@gmail.com
+# @File  : Test.py
+
+import webbrowser
+
+
+def main():
+    webbrowser.open_new('http://www.google.com')
+
+
+if __name__ == '__main__':
+    main()
